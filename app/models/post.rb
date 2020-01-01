77b434182a3f2986eb1c2ef6c5495b6551d66916
hectortllo <hectortllo@gmail.com>
2019-12-31 20:28:17 -0600

    Implemented model validations using TDD

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  #Llamando a las pruebas hechas en el archivo post_spec.rb
+  validates :title, presence: :true
+  validates :content, presence: :true
+  validates :published, presence: :true
+  validates :user_id, presence: :true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  #Dándole contexto a la prueba
+  describe "validations" do
+    #Definir la prueba en sí
+    it "valide presence of required fields" do
+      should validate_presence_of(:email)
+      should validate_presence_of(:name)
+      should validate_presence_of(:auth_token)
+    end
+
+    it "validate relation" do
+      should have_many(:posts)
+    end
+  end
 end
