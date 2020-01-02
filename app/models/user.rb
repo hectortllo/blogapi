df151ef38d26230a71e3b868b1ef1d18af80059a
hectortllo <hectortllo@gmail.com>
2020-01-02 12:24:15 -0600

    1. A new file was created for the logic of the creation of authentication tokens for the user. 2. Already have the logic to create an authentication token for each new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
     validates :name, presence: :true
     validates :auth_token, presence: :true
 
+    #Después de crear un objeto de tipo user, se ejecutarán
+    #los siguientes métodos
+    after_initialize :generate_auth_token
+
+    def generate_auth_token
+        #if !auth_token.present?
+        unless auth_token.present?
+            #generate token
+            self.auth_token = TokenGenerationService.generate
+        end
+    end
 end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -3,6 +3,5 @@ FactoryBot.define do
     #Se utilizará la gema 'Faker' para crear emails ficticios
     email { Faker::Internet.email }
     name { Faker::Name.name }
-    auth_token { "xxxxx" }
   end
 end
