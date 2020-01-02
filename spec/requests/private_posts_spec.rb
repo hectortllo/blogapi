920265135d82af3506b46ad84acee0fda141c647
hectortllo <hectortllo@gmail.com>
2020-01-02 12:07:22 -0600

    1. A new file was created on spec/requests/private_posts_spec.rb. In this file there will be tests for the private posts 2. The first part of the TDD has been done

---
+++ spec/requests/private_posts_spec.rb
@@ -0,0 +1,72 @@
+require "rails_helper"
+
+RSpec.describe "Posts with authentication", type: :request do
+
+    #Creando usuarios y posts de prueba
+    let!(:user) { create(:user) }
+    let!(:other_user) { create(:user) }
+    let!(:user_post) { create(:post, user_id: user.id) }
+    let!(:other_user_post) { create(:post, 
+        user_id: other_user.id, published: true) }
+    let!(:other_user_post_draft) { create(:post, 
+        user_id: other_user.id, published: false) }
+    #Headers de autenticación
+    let!(:auth_headers) { { 'Authorization' => 
+        "Bearer #{user.auth_token}" } }
+    let!(:other_auth_headers) { { 'Authorization' => 
+        "Bearer #{other_user.auth_token}" } }
+
+    #Prueba para el detalle de posts
+    describe "GET /posts/{id}" do
+        context "with valid auth" do
+            context "when requisting other's author post" do
+                context "when post is public" do
+                    before { get "/posts/#{other_user_post.id}", 
+                    headers: auth_headers }
+                    context "payload" do
+                        subject { JSON.parse(response.body) }
+                        #Validación para el payload
+                        it { is_expected.to include(:id) }
+                    end
+                    #Respuesta
+                    context "response" do
+                        subject { response } 
+                        it { is_expected.to have_http_status(:ok) }
+                    end
+                end
+                context "when post is draft" do
+                    before { get "/posts/#{other_user_post_draft.id}", 
+                    headers: auth_headers }
+                    context "payload" do
+                        subject { JSON.parse(response.body) }
+                        #Validación para el payload
+                        it { is_expected.to include(:error) }
+                    end
+                    #Respuesta
+                    context "response" do
+                        subject { response } 
+                        it { is_expected.to have_http_status(:not_found) }
+                    end
+                end
+            end
+
+            context "when requisting user's post" do
+            end
+        end
+    end
+
+    #Prueba para la creación de posts
+    describe "POST /posts" do
+    end
+
+    #Prueba para la actualización de posts
+    describe "PUT /posts" do
+    end
+
+    # it "should return OK" do
+    #     get '/posts'
+    #     payload = JSON.parse(response.body)
+    #     expect(payload).to be_empty
+    #     expect(response).to have_http_status(200)
+    # end
+end
