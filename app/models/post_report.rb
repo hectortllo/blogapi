3ab736a4177d97a7806af6e93a7e76751626f6af
hectortllo <hectortllo@gmail.com>
2020-01-02 18:54:06 -0600

    Implemented the Background Job using ActiveJobs for sending an report by email

---
+++ app/models/post_report.rb
@@ -0,0 +1,22 @@
+class PostReport < Struct.new(:word_count, :word_histogram)
+  #Histograma -> Guardará cuántas veces aparece repetida una palabra
+  def self.generate(post)
+    PostReport.new(
+      #word_count
+      #Reemplazar signos de puntuación
+      post.content.split.map { |word| word.gsub(/\W/,'') }.count
+      #word_histogram
+      calc_histogram(post)
+    )
+  end
+
+  def self.calc_histogram(post)
+    (post
+      .content
+      .split
+      .map { |word| word.gsub(/\W/,'') }
+      .map(&:downcase)
+      .group_by { |word| word })
+      .transform_values(&:size)
+  end
+end
